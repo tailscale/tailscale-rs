060234130d6a93d1d615d8f34fa15977b702cc32
Nathan Perry <nathan@tailscale.com>
2026-03-25 11:58:01 -0400

    netcheck: icmp prober
    Signed-off-by: Nathan Perry <nathan@tailscale.com>
    Change-Id: Ib15f6d14fe9e5d9f125d753d35ca3dfe6a6a6964

--- ts_netcheck/src/lib.rs
+++ ts_netcheck/src/lib.rs
@@ -2,9 +2,11 @@
 
 mod derp_latency;
 pub mod https;
+mod icmp;
 mod stun;
 
 pub use derp_latency::{Config, RegionResult, measure_derp_map};
 #[doc(inline)]
 pub use https::measure_https_latency;
+pub use icmp::IcmpProber;
 pub use stun::StunProber;
