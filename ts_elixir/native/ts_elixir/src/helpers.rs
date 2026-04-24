f442bbd32db150a8e16be91ddfc5b25c071efe2f
Nathan Perry <nathan@tailscale.com>
2026-04-24 07:50:03 -0400

    elixir: refactor to avoid use of dirtyio
    Potentially-blocking Rust-side calls now use message passing to respond
    to the caller. Elixir now has `Tailscale.Util.await`, which spawns a
    task that listens for the relevant response.
    
    Signed-off-by: Nathan Perry <nathan@tailscale.com>
    Change-Id: I703386221291fa7511e593acb8c3d0416a6a6964

--- ts_elixir/native/ts_elixir/src/helpers.rs
+++ ts_elixir/native/ts_elixir/src/helpers.rs
@@ -1,10 +1,8 @@
-use std::{error::Error, fmt::Display, net::SocketAddr};
+use std::{fmt::Display, net::SocketAddr};
 
-use rustler::{Encoder, NifResult, ResourceArc, Term};
+use rustler::{NifResult, ResourceArc};
 
-use crate::{atoms, erl_ip::ErlIp};
-
-pub type Result<T> = std::result::Result<T, Box<dyn Error + Send + Sync + 'static>>;
+use crate::erl_ip::ErlIp;
 
 /// Wrap the given [`rustler::Resource`] in a [`ResourceArc`] inside a [`NifResult`].
 pub fn ok_arc<T>(t: T) -> NifResult<ResourceArc<T>>
@@ -21,11 +19,4 @@ pub fn term_err(e: impl Display) -> rustler::Error {
 
 pub fn sockaddr_to_erl(addr: SocketAddr) -> (ErlIp, u16) {
     (ErlIp(addr.ip()), addr.port())
-}
-
-pub fn erl_result(env: rustler::Env, r: Result<impl Encoder>) -> NifResult<Term> {
-    match r {
-        Ok(t) => Ok((atoms::ok(), t).encode(env)),
-        Err(e) => Err(term_err(e)),
-    }
 }
