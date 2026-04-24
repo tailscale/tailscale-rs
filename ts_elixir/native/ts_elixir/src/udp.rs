b11d9b8ec081af38c8b77f04fad534b687e649b3
Nathan Perry <nathan@tailscale.com>
2026-04-24 07:50:03 -0400

    elixir: refactor to avoid use of dirtyio
    Potentially-blocking Rust-side calls now use message passing to respond
    to the caller. Elixir now has `Tailscale.Util.await`, which spawns a
    task that listens for the relevant response.
    
    Signed-off-by: Nathan Perry <nathan@tailscale.com>
    Change-Id: I703386221291fa7511e593acb8c3d0416a6a6964

--- ts_elixir/native/ts_elixir/src/udp.rs
+++ ts_elixir/native/ts_elixir/src/udp.rs
@@ -1,9 +1,11 @@
 use std::sync::Arc;
 
-use rustler::{Binary, Encoder, NifResult, ResourceArc, Term};
+use rustler::{Binary, Encoder, NifResult, ResourceArc};
+use tap::Pipe;
 
 use crate::{
-    Device, IpOrSelf, Result, TOKIO_RUNTIME, atoms, erl_ip::ErlIp, helpers::term_err, ok_arc,
+    AsyncReply, Device, IpOrSelf, erl_ip::ErlIp, helpers::term_err, ok_arc, sockaddr_to_erl,
+    try_reply_async,
 };
 
 pub struct UdpSocket {
@@ -13,56 +15,59 @@ pub struct UdpSocket {
 #[rustler::resource_impl]
 impl rustler::Resource for UdpSocket {}
 
-#[rustler::nif(schedule = "DirtyIo")]
-fn udp_bind(
-    env: rustler::Env,
+#[rustler::nif]
+fn udp_bind<'e>(
+    env: rustler::Env<'e>,
     dev: ResourceArc<Device>,
     ip: IpOrSelf,
     port: u16,
-) -> NifResult<impl Encoder> {
+) -> NifResult<AsyncReply<'e>> {
     let dev = dev.inner.clone();
 
-    TOKIO_RUNTIME
-        .block_on(async move {
-            let addr = ip.resolve(&dev).await?;
-            let sock = dev.udp_bind((addr, port).into()).await.map_err(term_err)?;
+    try_reply_async(env, async move {
+        let addr = ip.resolve(&dev).await?;
+        let sock = dev.udp_bind((addr, port).into()).await.map_err(term_err)?;
 
-            ok_arc(UdpSocket {
-                inner: Arc::new(sock),
-            })
+        ok_arc(UdpSocket {
+            inner: Arc::new(sock),
         })
-        .map(|sock| sock.encode(env))
+    })
+    .pipe(Ok)
 }
 
-#[rustler::nif(schedule = "DirtyIo")]
-fn udp_send<'env>(
-    env: rustler::Env<'env>,
+#[rustler::nif]
+fn udp_send<'e>(
+    env: rustler::Env<'e>,
     sock: ResourceArc<UdpSocket>,
-    ip: ErlIp,
+    addr: ErlIp,
     port: u16,
     msg: Binary,
-) -> Term<'env> {
+) -> NifResult<AsyncReply<'e>> {
     let msg = msg.to_vec();
     let sock = sock.inner.clone();
 
-    match TOKIO_RUNTIME.block_on(async move {
-        sock.send_to((ip.0, port).into(), &msg).await?;
-
-        Result::<_>::Ok(())
-    }) {
-        Ok(_) => atoms::ok().encode(env),
-        Err(e) => (atoms::error(), e.to_string()).encode(env),
-    }
+    try_reply_async(env, async move {
+        sock.send_to((addr, port).into(), &msg)
+            .await
+            .map(|_| ())
+            .map_err(term_err)
+    })
+    .pipe(Ok)
 }
 
-#[rustler::nif(schedule = "DirtyIo")]
-fn udp_recv(env: rustler::Env, sock: ResourceArc<UdpSocket>) -> NifResult<Term> {
-    let (who, msg) = sock.inner.recv_from_bytes_blocking().map_err(term_err)?;
+#[rustler::nif]
+fn udp_recv(env: rustler::Env, sock: ResourceArc<UdpSocket>) -> AsyncReply {
+    let sock = sock.inner.clone();
 
-    Ok((atoms::ok(), ErlIp(who.ip()), who.port(), msg.to_vec()).encode(env))
+    try_reply_async(env, async move {
+        sock.recv_from_bytes()
+            .await
+            .map(|(s, msg)| (ErlIp(s.ip()), s.port(), msg.to_vec()))
+            .map_err(term_err)
+    })
 }
 
 #[rustler::nif]
 fn udp_local_addr(sock: ResourceArc<UdpSocket>) -> impl Encoder {
-    crate::sockaddr_to_erl(sock.inner.local_addr())
+    sockaddr_to_erl(sock.inner.local_addr())
 }
