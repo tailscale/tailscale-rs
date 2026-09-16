ef25c8097dc4489c1a67907123737ca8b0cf2aba
Dylan Bargatze <dylan@tailscale.com>
2026-09-16 17:31:36 -0400

    ts_control_serde: add omit_default_region parsing test
    Integration test on cut-down DerpMap to ensure we correctly parse/populate `DerpMap::omit_default_regions`.
    
    Signed-off-by: Dylan Bargatze <dylan@tailscale.com>

--- ts_control_serde/tests/parsing_tests.rs
+++ ts_control_serde/tests/parsing_tests.rs
@@ -1,6 +1,6 @@
 #![allow(missing_docs)]
 
-use ts_control_serde::{MapResponse, Node};
+use ts_control_serde::{DerpMap, MapResponse, Node};
 
 #[test]
 fn test_map_response_parse() {
@@ -17,4 +17,17 @@ fn test_node_parse() {
 
     let node = serde_json::from_str::<Node>(TEST_NODE).expect("could not parse Node");
     println!("{node:#?}");
+}
+
+/// Validates that a `DerpMap` with "omitDefaultRegions" set is properly parsed.
+#[test]
+fn test_derp_map_omit_default_regions() {
+    const TEST_DERP_MAP: &str = include_str!("data/derp_map_odr.json");
+
+    let map = serde_json::from_str::<DerpMap>(TEST_DERP_MAP)
+        .expect("could not parse DerpMap with omit_default_regions");
+    assert!(
+        map.omit_default_regions,
+        "omit_default_regions should be true"
+    );
 }
